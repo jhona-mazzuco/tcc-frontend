86fa731b9cad7cf326083af8ef5f3120e08f5ce2
Jhonattan Loli Mazzuco <jhonattanloli@gmail.com>
2022-11-30 18:52:55 -0300

    Atualiza para o novo aplicativo firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 
 export const environment = {
   production: true,
-  api: "https://us-central1-tcc-front-bacdb.cloudfunctions.net",
+  api: "https://us-central1-vilson-centro-esportivo.cloudfunctions.net",
   firebase: {
-    apiKey: "AIzaSyA1tDlKDKWKsNWBqJaUjbPo-qu3YhbS_BY",
-    authDomain: "tcc-front-bacdb.firebaseapp.com",
-    projectId: "tcc-front-bacdb",
-    storageBucket: "tcc-front-bacdb.appspot.com",
-    messagingSenderId: "839388945354",
-    appId: "1:839388945354:web:deca1b82cb7c27ce914d33",
+    apiKey: "AIzaSyDsoOz2FytAXCVf2lVaCBH6zdZOyiscDjg",
+    authDomain: "vilson-centro-esportivo.firebaseapp.com",
+    projectId: "vilson-centro-esportivo",
+    storageBucket: "vilson-centro-esportivo.appspot.com",
+    messagingSenderId: "368664646679",
+    appId: "1:368664646679:web:b63b74c9f48ca9ed647852",
   }
 };
   
